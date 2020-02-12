321ef0bd1177b6dc3c6a74ec994a6ebc33601bf1
showwin <showwin_kmc@yahoo.co.jp>
2020-02-12 13:28:08 +0900

    [add] 最終閲覧日時の情報を追加

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -13,18 +13,19 @@ function addCounter() {
   $('div[class^="SearchForm__InputBase"]').append("<div style='text-align: right; padding-right: 27px; margin-top: 7px;'><p id='viewed-count'></p></div>")
 }
 
-function displayTime(time) {
-  if (!time) {
+function displayTime(timestamp) {
+  if (!timestamp) {
     return ''
   }
+  const time = new Date(timestamp)
   const year = time.getFullYear();
   const mon = time.getMonth() + 1; //１を足すこと
   const day = time.getDate();
   const hour = time.getHours();
-  const min = time.getMinutes();
-  const sec = time.getSeconds();
+  const min = ('00' + time.getMinutes()).slice(-2); // ゼロ埋め
+  const sec = ('00' + time.getSeconds()).slice(-2); // ゼロ埋め
 
-  const s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分"
+  const s = year + "年" + mon + "月" + day + "日 " + hour + ":" + min
   return s
 }
 
@@ -32,54 +33,63 @@ function getViewedUserKey(userID) {
   return 'vw-' + userID
 }
 
-function storeLastViewed(userID) {
-  // 最新の閲覧日時をみて、それが1時間以上経っている場合のみ保存する
-  // 最も新しいもの2つを保存する (なぜなら、今見ている時間を保存する必要がありつつ、表示には前回のものを使うため)
-  const key = getViewedUserKey(userID)
-  const latestTimestamp = _getLatestViewedTimestamp(key)
-  const now = new Date().getTime()
-  console.log('compare: ' + now + ' vs ' + latestTimestamp)
-  console.log('sub: ' + (now - latestTimestamp))
-  if ((now - latestTimestamp) > VIEWED_TIME_THRESHOLD){
-    var storeObj = {}
-    storeObj[key] = [now, latestTimestamp]
-    console.log('save: ')
-    console.log(storeObj)
-    chrome.storage.local.set(storeObj)
-  }
+
+function saveTimeStamp(key, newerTime, olderTIme) {
+  var storeObj = {}
+  storeObj[key] = [newerTime, olderTIme]
+  chrome.storage.local.set(storeObj)
+
 }
 
-function _getLatestViewedTimestamp(viewedKey) {
-  // 最も新しい閲覧時間を取得
-  var latestTimestamp = null
+function storeLastViewed(userID) {
+  // 最新の閲覧日時をみて、それが VIEWED_TIME_THRESHOLD 以上経っている場合のみ保存する
+  // 最も新しいもの2つを保存する (なぜなら、今見ている時間を保存する必要がありつつ、表示には前回のものを使うため)
+  const viewedKey = getViewedUserKey(userID)
+  const now = new Date().getTime()
   chrome.storage.local.get([viewedKey], function (result) {
-    if (result[viewedKey]) {
-      latestTimestamp = result[viewedKey][0]
-      console.log('_getLatestViewedTimestamp 1: ' + latestTimestamp)
+    if (result[viewedKey] !== undefined) {
+      const latestTimestamp = result[viewedKey][0]
+
+      if ((now - latestTimestamp) > VIEWED_TIME_THRESHOLD){
+        console.log('save: update')
+        saveTimeStamp(viewedKey, now, latestTimestamp)
+      }
+    } else {
+      console.log('save: now')
+      saveTimeStamp(viewedKey, now, null)
     }
   })
-  console.log('_getLatestViewedTimestamp 2: ' + latestTimestamp)
-  return latestTimestamp
 }
 
-function getLastViewed(userID) {
+function showLastViewed(card, userID) {
   // 前回閲覧した時間を取得
   // 1時間以上前に見た記録に限って取得する
-  var latestTimestamp = null
   const now = new Date()
   const key = getViewedUserKey(userID)
   chrome.storage.local.get([key], function (result) {
-    if (result[key]) {
+    if (result[key] !== undefined) {
+      console.log("showLastViewed")
       const last = result[key][0]
       const last2 = result[key][1]
       if ((now - last) > VIEWED_TIME_THRESHOLD){
-        latestTimestamp = last
+        appendLastViewed(card, last)
       } else {
-        latestTimestamp = last2
+        appendLastViewed(card, last2)
       }
     }
   })
-  return latestTimestamp
+}
+
+function appendLastViewed(parentElm, timestamp) {
+  const text = displayTime(timestamp)
+  const lastViewedParentElm = $(parentElm).find('div[class^="UserListItemTypeCard__ScoutButtonContainer"]')
+  const isExist = $(lastViewedParentElm).find('span.LastViewed')
+  if ($(isExist).length === 0 && text !== '') {
+    $(lastViewedParentElm).prepend("<span class='LastViewed' style='font-weight: 400; font-size: 12px; color: rgba(0, 0, 0, 0.4); line-height: 16px;'>最終閲覧日時: " + text + "</span>")
+  }
+  // TODO: 最後に詳細画面を開いた 「詳細確認日時」を最終閲覧日時の下に出す
+  // TODO: <div>で囲って<br>することで2行にして出す
+  // TODO: それぞれの言葉の意味はtooltipでだす。(すぐ右のボタンがtooltopを使っている)
 }
 
 // 最初の要素は値がズレがちでIDとして扱うとバグるので、最初の5つ(<4000)は固定でいれる
@@ -99,11 +109,8 @@ $(window).scroll(function(){
     }
     // カードのIDに `UserListItem-18411406` が入っているのでUserIDだけ抜き出す
     const userID = $(card).children('div').first().attr('id').slice(13, -1)
-    const lastViewTimestamp = getLastViewed(userID)
+    showLastViewed(card, userID)
     storeLastViewed(userID)
-
-    const a = displayTime(lastViewTimestamp)
-    console.log(a)
   })
 
   var denom = $('p[class^="UserListContainer__HeadingNumberLabel"]').text()
